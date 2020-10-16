7603b991c4ba05f4c0e96c7fdb1aa36824da3049
Akhil <akhilreddy18@outlook.com>
2020-10-16 19:16:47 +0200

    add css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
+import './index.css';
 
 // core components
 import Admin from "./layouts/Admin.js";
