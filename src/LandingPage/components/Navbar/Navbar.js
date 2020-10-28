ef34d008538fb5f189070c1a4c1e075435c56bf0
Akhil <akhilreddy18@outlook.com>
2020-10-28 22:09:40 +0100

    add bootstrap modal

--- src/LandingPage/components/Navbar/Navbar.js
+++ src/LandingPage/components/Navbar/Navbar.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Nav, Navbar, Form, Button } from "react-bootstrap";
+import { Nav, Navbar, Form, Button, Modal, Col, Row } from "react-bootstrap";
 import classes from "./Navbar.module.css";
+import { useState } from "react";
 
-const navbar = () => {
+const NavbarComponent = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <Navbar className={classes.navbar} expand="lg">
@@ -20,14 +26,50 @@ const navbar = () => {
             </Nav.Link>
           </Nav>
           <Form inline>
-            <Button className={classes.loginButton}>LOG IN</Button>
+            <Button onClick={handleShow} className={classes.loginButton}>LOG IN</Button>
             <Button className={classes.tryButton}>TRY IT FOR FREE</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="justify-content-around">
+            <div style={{border: "2px solid"}}>
+            <Col>
+            I will not close if you click outside me. 
+            </Col>
+            </div>
+            <div style={{border: "2px solid"}}>
+            <Col>
+            Don't even try to press
+          escape key.
+            </Col>
+            </div>
+          </Row>
+        </Modal.Body>
+        {/* <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer> */}
+      </Modal>
     </>
   );
 };
+
+function handleLogin() {
+  alert("clicked");
+}
 
 const style = {
   brand: {
@@ -42,4 +84,4 @@ const style = {
   },
 };
 
-export default navbar;
+export default NavbarComponent;
